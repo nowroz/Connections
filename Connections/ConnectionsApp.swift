7d1d7e6159e9b86978af89e2356a52781fb280e6
Nowroz Islam <116680399+nowroz@users.noreply.github.com>
2024-09-24 06:23:29 +0600

    Initial Commit
    The project at this stage only containes the boilerplate code.

---
+++ Connections/ConnectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectionsApp.swift
+//  Connections
+//
+//  Created by Nowroz Islam on 24/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ConnectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
